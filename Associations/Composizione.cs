5f1eb045c36c74e2f448e4142b1d5303a00031a9
Bruno Ferreira <bruno.ferreira.rocha@gmail.com>
2023-03-10 12:43:42 +0100

    Lezione associazioni - 10/03/23

---
+++ Associations/Composizione.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Associations.Composizione
+{
+    class Company
+    {
+        CEO _cEO;
+        public Company(string CeoName)
+        {
+            _cEO = new CEO(CeoName);
+
+        }
+        class CEO
+        {
+            string _name;
+            public CEO(string name)
+            {
+                _name = name;
+            }
+        }
+    }
+
+}
